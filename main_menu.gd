b67cd9da9a5d442035248e393ce02f27bf5cefa6
Nicolas zoppi <nicolas.zoppi@etu.univ-amu.fr>
2026-06-20 22:16:30 +0200

    fix:Passage à godot 4.7

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://assets/scenes/node_2d.tscn")
+	get_tree().change_scene_to_file("res://assets/scenes/scene1.tscn")
 
 
 func _on_quit_pressed() -> void:
