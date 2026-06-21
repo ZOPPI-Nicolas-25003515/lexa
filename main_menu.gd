0d1745300c01c1b4604306b051d908f07fbbd2ce
BULUT-Melissa-25006748 <melissa.bulut@etu.univ-amu.fr>
2026-06-21 03:59:33 +0200

    feat(melissa): Ajout du menu complet

--- main_menu.gd
+++ main_menu.gd
@@ -3,6 +3,8 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/scenes/scene1.tscn")
 
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://assets/scenes/options_menu.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
